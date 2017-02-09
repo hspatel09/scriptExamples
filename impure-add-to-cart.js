7f21064fb05ce40207a80ad656d3c6b3fc501593
hspatel09 <patel.himanshu.s@gmail.com>
2017-02-09 11:34:02 -0500

    Create impure-add-to-cart.js

---
+++ impure-add-to-cart.js
@@ -0,0 +1,32 @@
+// impure addToCart mutates existing cart
+const addToCart = (cart, item, quantity) => {
+  cart.items.push({
+    item,
+    quantity
+  });
+  return cart;
+};
+
+
+test('addToCart()', assert => {
+  const msg = 'addToCart() should add a new item to the cart.';
+  const originalCart =     {
+    items: []
+  };
+  const cart = addToCart(
+    originalCart,
+    {
+      name: "Digital SLR Camera",
+      price: '1495'
+    },
+    1
+  );
+
+  const expected = 1; // num items in cart
+  const actual = cart.items.length;
+
+  assert.equal(actual, expected, msg);
+
+  assert.deepEqual(originalCart, cart, 'mutates original cart.');
+  assert.end();
+});
